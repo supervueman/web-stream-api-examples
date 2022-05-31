ba8bde19fb98e4de993e346aa2e7172b07e8c291
supervueman <supervueman@gmail.com>
2022-05-31 22:34:35 +0400

    Get stream

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,3 +2,24 @@
 import './vendor/jquery'
 import './vendor/bootstrap'
 import './vendor/fontawesome'
+
+window.addEventListener('load', () => {
+  let supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+
+  console.log(supportedConstraints)
+
+  navigator.mediaDevices.getUserMedia({
+    video: {
+      width: { min: 640, ideal: 1920 },
+      height: { min: 400, ideal: 1080 },
+      aspectRatio: { ideal: 1.7777777778 }
+    },
+    audio: {
+      sampleSize: 16,
+      channelCount: 2
+    }
+  }).then(stream => {
+    console.log(stream)
+    videoElement.srcObject = stream;
+  })
+})
