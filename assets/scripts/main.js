890375b77659f880c80eb4341cde44546e48df5b
supervueman <supervueman@gmail.com>
2022-06-02 22:28:37 +0400

    Styles

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,30 +4,35 @@ import './vendor/bootstrap'
 import './vendor/fontawesome'
 
 window.addEventListener('load', () => {
-  let supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+  const btnRecord = $('.btn-record')
+  // const btnStop = $('.btn-stop')
 
-  const constraints = {
-    video: {
-      width: { max: 640, ideal: 1920 },
-      height: { min: 400, ideal: 1080 },
-      aspectRatio: { ideal: 1.7777777778 }
-    },
-    audio: {
-      sampleSize: 16,
-      channelCount: 2
+  btnRecord.on('click', () => {
+    let supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+
+    const constraints = {
+      video: {
+        width: { max: 640, ideal: 1920 },
+        height: { min: 400, ideal: 1080 },
+        aspectRatio: { ideal: 1.7777777778 }
+      },
+      audio: {
+        sampleSize: 16,
+        channelCount: 2
+      }
     }
-  }
 
-  navigator.mediaDevices.enumerateDevices().then(devices => {
-    console.log(devices)
-  })
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+      console.log(devices)
+    })
 
-  console.log(supportedConstraints)
+    console.log(supportedConstraints)
 
-  navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-    console.log(stream.getVideoTracks()[0].getCapabilities())
-    const videoElement = document.querySelector('#video')
-    videoElement.srcObject = stream;
-    videoElement.play()
+    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+      console.log(stream.getVideoTracks()[0].getCapabilities())
+      const videoElement = document.querySelector('#video')
+      videoElement.srcObject = stream;
+      videoElement.play()
+    })
   })
 })
